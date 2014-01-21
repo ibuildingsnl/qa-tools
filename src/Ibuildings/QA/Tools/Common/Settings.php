f856c42f11eeef8ac5910d0e798aedc9e797f9b6
Martin Roest <martin@ibuildings.nl>
2014-01-21 14:10:12 +0100

    Persistent configuration object to keep your settings across app runs.

--- src/Ibuildings/QA/Tools/Common/Settings.php
+++ src/Ibuildings/QA/Tools/Common/Settings.php
@@ -2,8 +2,15 @@
 
 namespace Ibuildings\QA\Tools\Common;
 
+use Camspiers\JsonPretty\JsonPretty;
+
 class Settings extends \ArrayObject
 {
+    /**
+     * @var string
+     */
+    protected $filename = 'settings.json';
+
     /**
      * Instantiate the object.
      *
@@ -13,7 +20,6 @@ class Settings extends \ArrayObject
      */
     public function __construct($baseDir, $packageBaseDir)
     {
-
         if (!is_dir($packageBaseDir)) {
             throw new \Exception('Cannot find vendor package dir:' . $packageBaseDir);
         }
@@ -22,8 +28,37 @@ class Settings extends \ArrayObject
             throw new \Exception('Cannot find project base dir:' . $baseDir);
         }
 
-        $this['packageBaseDir'] = $packageBaseDir;
-        $this['baseDir'] = $baseDir;
+        $this->packageBaseDir = $packageBaseDir;
+        $this->baseDir = $baseDir;
+
+        // Load settings from this (if any)
+        $this->initializeFromConfig();
+    }
+
+    /**
+     * Load configuration from settings json file
+     */
+    protected function initializeFromConfig()
+    {
+        $configurationFile = $this->configurationFile();
+        if (is_readable($configurationFile)) {
+            $loadedConfiguration = json_decode(file_get_contents($configurationFile));
+            $this->exchangeArray($loadedConfiguration);
+        }
+    }
+
+    /**
+     * File path
+     *
+     * @return string
+     */
+    protected function configurationFile()
+    {
+        $configurationFile = $this->getBaseDir()
+            . DIRECTORY_SEPARATOR
+            . '/'
+            . $this->filename;
+        return $configurationFile;
     }
 
     /**
@@ -31,7 +66,7 @@ class Settings extends \ArrayObject
      */
     public function getBaseDir()
     {
-        return $this['baseDir'];
+        return $this->baseDir;
     }
 
     /**
@@ -39,6 +74,26 @@ class Settings extends \ArrayObject
      */
     public function getPackageBaseDir()
     {
-        return $this['packageBaseDir'];
+        return $this->packageBaseDir;
+    }
+
+    /**
+     * Destructor. Will write app config to file
+     * to keep configuration persistent across app
+     * runs.
+     *
+     * @todo throw a warning when file is not writable.
+     */
+    public function __destruct()
+    {
+        $configurationFile = $this->configurationFile();
+
+        if (is_writable($this->getBaseDir()) &&
+            (!file_exists($configurationFile) OR is_writable($configurationFile))
+        ) {
+            $jsonPretty = new JsonPretty();
+            $json = $jsonPretty->prettify($this->getArrayCopy());
+            file_put_contents($configurationFile, $json);
+        }
     }
 }
