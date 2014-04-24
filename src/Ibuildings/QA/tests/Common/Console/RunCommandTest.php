47ea2ea2aa7e01ebc93df2ad9520198d0e26f0a9
DRvanR <dvrenterghem@ibuildings.nl>
2014-04-24 12:40:11 +0200

    Bumping to version 1.1.15

--- src/Ibuildings/QA/tests/Common/Console/RunCommandTest.php
+++ src/Ibuildings/QA/tests/Common/Console/RunCommandTest.php
@@ -42,7 +42,7 @@ class RunCommandTest extends \PHPUnit_Framework_TestCase
         $preCommitHookCommand = new RunCommand();
         $preCommitHookCommand->setChecker($checker);
 
-        $this->application = new Application('ibuildings qa tools', '1.1.14', $settings);
+        $this->application = new Application('ibuildings qa tools', '1.1.15', $settings);
 
         $this->application->add($preCommitHookCommand);
     }
