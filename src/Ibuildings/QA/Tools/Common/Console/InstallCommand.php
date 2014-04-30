6ec9f63459adb327ccf8cdaaa3c530a43b91f27a
DRvanR <dvrenterghem@ibuildings.nl>
2014-04-30 16:43:09 +0200

    Adding support for --no-interaction

--- src/Ibuildings/QA/Tools/Common/Console/InstallCommand.php
+++ src/Ibuildings/QA/Tools/Common/Console/InstallCommand.php
@@ -77,6 +77,24 @@ class InstallCommand extends AbstractCommand
 
         $this->dialog = $this->getApplication()->getDialogHelper();
         $this->filesystem = new Filesystem();
+
+        if ($input->isInteractive()) {
+            return;
+        }
+
+        // if the qa-tools are loaded, we support the --no-interaction (-n) flag. This is achieved by
+        // making the dialog input-aware, see \Symfony\Console\Helper\DialogHelper::ask() lines 101-103 and
+        // \Symfony\Console\Helper\InputAwareHelper
+        if (!$this->settings->hasLoadedJsonFile() || !$this->settings->previousRunWasCompleted()) {
+            $output->writeln(
+                '<error>Previous run was not completed fully, cannot run in non-interactive mode</error>'
+            );
+
+            // stop with error
+            exit(1);
+        }
+
+        $this->dialog->setInput($input);
     }
 
     /**
@@ -166,6 +184,8 @@ class InstallCommand extends AbstractCommand
 
         $command = $this->getApplication()->find('install:pre-commit');
         $command->run($input, $output);
+
+        $this->settings['_qa_tools_run_completed'] = true;
     }
 
     /**
