629a22a990778bd0569adab1bac1ad3e558f3539
Alex Rothuis <arothuis@ibuildings.nl>
2016-05-03 12:26:17 +0200

    Introduce own Application

---
+++ src/Core/Application/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ibuildings\QaTools\Core\Application;
+
+use Symfony\Component\Console\Application as ConsoleApplication;
+
+final class Application extends ConsoleApplication
+{
+}
