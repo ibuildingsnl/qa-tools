b44305d3f1af43d3421181a7f043a5d4050fbac7
Alex Rothuis <arothuis@ibuildings.nl>
2016-05-04 17:22:30 +0200

    Add Composer script for building phars, formatting
    Using Composer for this will give us more control over the exact steps to be taken when building.
    For instance, we could export a separate hash-file after building if we wanted to.
    
    Also, we could give the user some information about setting phar.readonly setting to "Off" and
    what ini-file should be edited.

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -9,29 +9,33 @@ final class ScriptHandler
 {
     public static function requireBoxPhar(Event $event)
     {
-        $io = $event->getIO();
+        $io     = $event->getIO();
         $config = $event->getComposer()->getPackage()->getExtra();
 
-        $source = $config['qa-tools-box-source'];
+        $source      = $config['qa-tools-box-source'];
         $installPath = $config['qa-tools-box-install-path'];
-        $shaSum = $config['qa-tools-box-sha-sum'];
+        $shaSum      = $config['qa-tools-box-sha-sum'];
 
         if (!$event->isDevMode()) {
             return $io->write('<warning>Not downloading Box in --no-dev mode</warning>');
         }
 
         if (file_exists($installPath) && sha1_file($installPath) === $shaSum) {
-            return $io->write(sprintf(
-                '<warning>Expected version of Box is already installed in "%s"</warning>',
-                $installPath
-            ));
+            return $io->write(
+                sprintf(
+                    '<warning>Expected version of Box is already installed in "%s"</warning>',
+                    $installPath
+                )
+            );
         }
 
         if (!is_writable(dirname($installPath))) {
-            throw new Exception(sprintf(
-                'Cannot download Box to "%s": directory is not writable',
-                dirname($installPath)
-            ));
+            throw new Exception(
+                sprintf(
+                    'Cannot download Box to "%s": directory is not writable',
+                    dirname($installPath)
+                )
+            );
         }
 
         $io->write(sprintf('<info>Downloading Box from "%s"</info>', $source));
@@ -40,19 +44,23 @@ final class ScriptHandler
         if ($boxContents === false) {
             $lastError = error_get_last();
 
-            throw new Exception(sprintf(
-                'Could not download Box from "%s": %s',
-                $source,
-                $lastError['message']
-            ));
+            throw new Exception(
+                sprintf(
+                    'Could not download Box from "%s": %s',
+                    $source,
+                    $lastError['message']
+                )
+            );
         }
 
         if (sha1($boxContents) !== $shaSum) {
-            throw new Exception(sprintf(
-                'Hash of downloaded Box ("%s") does not match expected hash ("%s").',
-                sha1($boxContents),
-                $shaSum
-            ));
+            throw new Exception(
+                sprintf(
+                    'Hash of downloaded Box ("%s") does not match expected hash ("%s").',
+                    sha1($boxContents),
+                    $shaSum
+                )
+            );
         }
 
         $result = file_put_contents($installPath, $boxContents);
@@ -60,13 +68,56 @@ final class ScriptHandler
         if ($result === false) {
             $lastError = error_get_last();
 
-            throw new Exception(sprintf(
-                'Could not save Box to: "%s": %s',
-                $installPath,
-                $lastError['message']
-            ));
+            throw new Exception(
+                sprintf(
+                    'Could not save Box to: "%s": %s',
+                    $installPath,
+                    $lastError['message']
+                )
+            );
         }
 
         return $io->write(sprintf('<info>Installed Box at "%s"</info>', $installPath));
+    }
+
+    public static function buildPhar(Event $event)
+    {
+        $config = $event->getComposer()->getPackage()->getExtra();
+
+        $pharIsReadOnly = ini_get('phar.readonly') ? true : false;
+
+        if ($pharIsReadOnly) {
+            throw new Exception(
+                sprintf(
+                    'Cannot build phar: the PHP Phar extension is configured to be read-only.'.PHP_EOL.
+                    'Please set phar.readonly to "Off" in your php.ini at: "%s"',
+                    php_ini_loaded_file()
+                )
+            );
+        }
+
+        if (!file_exists($config['qa-tools-box-install-path'])) {
+            throw new Exception(
+                sprintf(
+                    'Cannot build phar: file "%s" does not exist',
+                    $config['qa-tools-box-install-path']
+                )
+            );
+        }
+
+        if (!is_executable($config['qa-tools-box-install-path'])) {
+            throw new Exception(
+                sprintf(
+                    'Cannot build phar: file "%s" is not executable',
+                    $config['qa-tools-box-install-path']
+                )
+            );
+        }
+
+        $event->getIO()->write(
+            sprintf('<info>Building phar with "%s:"</info>', $config['qa-tools-box-install-path'].' build -vv')
+        );
+
+        passthru($config['qa-tools-box-install-path'].' build -vv');
     }
 }
