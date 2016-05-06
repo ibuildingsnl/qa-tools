89f9107114e78fd77dcffd5c5bfa98f68094eb45
Alex Rothuis <arothuis@ibuildings.nl>
2016-05-06 16:27:13 +0200

    Make commands implement ContainerAwareInterface

--- src/Core/Application/Command/PrePushCommand.php
+++ src/Core/Application/Command/PrePushCommand.php
@@ -5,9 +5,13 @@ namespace Ibuildings\QaTools\Core\Application\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-final class PrePushCommand extends Command
+final class PrePushCommand extends Command implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     protected function configure()
     {
         $this
