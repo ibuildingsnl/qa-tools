57a9a5df3b2d3a7ba9dc912ad36520ffb83680e4
Alex Rothuis <arothuis@ibuildings.nl>
2016-05-11 18:11:00 +0200

    Expand assertions, exceptions

--- src/Assert/Assertion.php
+++ src/Assert/Assertion.php
@@ -7,9 +7,7 @@ use Ibuildings\QaTools\Exception\InvalidArgumentException;
 
 final class Assertion extends BaseAssertion
 {
-    const INVALID_NON_EMPTY_STRING  = 1001;
-    const INVALID_STRING_OR_INTEGER = 1002;
-
+    const INVALID_NON_EMPTY_STRING   = 1001;
 
     protected static $exceptionClass = InvalidArgumentException::class;
 
@@ -22,6 +20,7 @@ final class Assertion extends BaseAssertion
     {
         if (!is_string($value) || trim($value) === '') {
             $message = 'Expected non-empty string for "%s", "%s" given';
+
             throw static::createException(
                 $value,
                 sprintf($message, $propertyPath, static::stringify($value)),
---
+++ src/Exception/LogicException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ibuildings\QaTools\Exception;
+
+use LogicException as CoreLogicException;
+
+class LogicException extends CoreLogicException
+{
+}
