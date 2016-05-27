85c8184240838bae0f4e7adcd3ef065eb3e02b29
Alex Rothuis <arothuis@ibuildings.nl>
2016-05-27 11:37:59 +0200

    Introduce a blank phpmd.sf2 configurator

--- src/Tool/PhpMd/Configurator/PhpMdSf2Configurator.php
+++ src/Tool/PhpMd/Configurator/PhpMdSf2Configurator.php
@@ -2,14 +2,23 @@
 
 namespace Ibuildings\QaTools\Tool\PhpMd\Configurator;
 
-use Ibuildings\QaTools\Core\ConfigurationBuilder\ConfigurationBuilder;
+use Ibuildings\QaTools\Core\Configuration\ConfigurationBuilder;
 use Ibuildings\QaTools\Core\Configurator\Configurator;
 use Ibuildings\QaTools\Core\Interviewer\Interviewer;
+use Ibuildings\QaTools\Tool\PhpMd\PhpMd;
 
 final class PhpMdSf2Configurator implements Configurator
 {
     public function configure(ConfigurationBuilder $configurationBuilder, Interviewer $interviewer)
     {
+        // Configure tool
+    }
 
+    /**
+     * @return string
+     */
+    public function getToolClassName()
+    {
+        return PhpMd::class;
     }
 }
