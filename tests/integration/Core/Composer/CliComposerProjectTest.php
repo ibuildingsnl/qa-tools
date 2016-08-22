40b1a539ebfdf4febf928232ae959c500906d890
Reinier Kip <rkip@ibuildings.nl>
2016-08-22 16:26:35 +0200

    Remove duplicate registration of emulated Composer packages

--- tests/integration/Core/Composer/CliComposerProjectTest.php
+++ tests/integration/Core/Composer/CliComposerProjectTest.php
@@ -45,7 +45,6 @@ class CliComposerProjectTest extends TestCase
     public function can_require_dev_dependencies()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
 
         $this->project->requireDevDependencies(new PackageSet([Package::of('phpmd/phpmd', '^2.0')]));
 
@@ -59,7 +58,6 @@ class CliComposerProjectTest extends TestCase
     public function can_get_the_current_composer_configuration_of_a_project_without_locked_deps()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
 
         $configuration = $this->project->getConfiguration();
         $this->assertContains('{', $configuration->getComposerJson());
@@ -70,7 +68,6 @@ class CliComposerProjectTest extends TestCase
     public function can_get_the_current_composer_configuration_of_a_project_with_locked_deps()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
 
         $this->project->requireDevDependencies(new PackageSet([Package::of('phpmd/phpmd', '^2.0')]));
 
@@ -82,7 +79,6 @@ class CliComposerProjectTest extends TestCase
     public function can_restore_a_composer_configuration()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
 
         $configurationBackup = $this->project->getConfiguration();
 
@@ -96,8 +92,6 @@ class CliComposerProjectTest extends TestCase
     public function can_add_conflicts()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
-
         Composer::addConflict('phpmd/phpmd', '^2.0');
 
         $this->expectException(RuntimeException::class);
@@ -109,8 +103,6 @@ class CliComposerProjectTest extends TestCase
     public function can_verify_dev_dep_wont_conflict()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
-
         Composer::addConflict('phpmd/phpmd', '^2.0');
 
         $this->expectException(RuntimeException::class);
@@ -122,7 +114,6 @@ class CliComposerProjectTest extends TestCase
     public function dev_dep_conflict_verification_doesnt_change_configuration()
     {
         Composer::initialise();
-        $this->setupLocalPackages();
 
         $expectedConfiguration = $this->project->getConfiguration();
         $this->project->verifyDevDependenciesWillNotConflict(new PackageSet([Package::of('phpmd/phpmd', '^2.0')]));
@@ -136,17 +127,5 @@ class CliComposerProjectTest extends TestCase
                 "Composer configuration was changed while verifying an added development dependency wouldn't conflict"
             )
         );
-    }
-
-    private function setupLocalPackages()
-    {
-        // Emulate all the tools' Composer packages locally to guarantee test
-        // reliability by removing the Internet factor and to speed up tests.
-        $configuration = json_decode(file_get_contents('composer.json'));
-        $configuration->repositories = [
-            ['packagist' => false],
-            ['type' => 'path', 'url' => __DIR__ . '/../../../composer/packages/phpmd'],
-        ];
-        file_put_contents('composer.json', json_encode($configuration, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
