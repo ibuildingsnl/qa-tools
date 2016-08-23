9d37face3e796bbaf22b4f90992089b9c74b3857
Reinier Kip <rkip@ibuildings.nl>
2016-08-23 16:11:07 +0200

    Allow escaping of strings to an XML context

---
+++ src/Core/Templating/TwigFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ibuildings\QaTools\Core\Templating;
+
+use Twig_Environment as Environment;
+use Twig_Extension_Core as CoreExtension;
+
+final class TwigFactory
+{
+    /**
+     * @return Environment
+     */
+    public static function create()
+    {
+        $twig = new Environment();
+
+        /** @var CoreExtension $coreExtension */
+        $coreExtension = $twig->getExtension('core');
+        $coreExtension->setEscaper('xml', [Escape::class, 'xml']);
+
+        return $twig;
+    }
+}
