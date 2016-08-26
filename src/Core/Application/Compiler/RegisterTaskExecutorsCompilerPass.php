8027888f4268643fa942dfb1721a1ead5b59121e
Reinier Kip <rkip@ibuildings.nl>
2016-08-26 12:01:57 +0200

    Allow specification of task execution priority

--- src/Core/Application/Compiler/RegisterTaskExecutorsCompilerPass.php
+++ src/Core/Application/Compiler/RegisterTaskExecutorsCompilerPass.php
@@ -2,6 +2,7 @@
 
 namespace Ibuildings\QaTools\Core\Application\Compiler;
 
+use Ibuildings\QaTools\Core\Exception\RuntimeException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -11,12 +12,57 @@ final class RegisterTaskExecutorsCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $executorExecutorDefinition = $container->findDefinition('qa_tools.task.task_directory_executor');
-        $taggedExecutorIds = array_unique(array_keys($container->findTaggedServiceIds('qa_tools.task_executor')));
-        $taggedExecutorReferences = array_map(
-            function ($serviceId) {
-                return new Reference($serviceId);
+
+        $taggedExecutors = $container->findTaggedServiceIds('qa_tools.task_executor');
+        $executorPriorityTuples = array_map(
+            function ($serviceId, $tags) {
+                if (count($tags) > 1) {
+                    throw new RuntimeException(
+                        sprintf(
+                            'Task executor "%s" may not be registered more than once (%d times), ' .
+                            'please check its service definition\'s tags',
+                            $serviceId,
+                            count($tags)
+                        )
+                    );
+                }
+                if (!isset($tags[0]['priority'])) {
+                    throw new RuntimeException(
+                        sprintf(
+                            'Tag "qa_tools.task_executor" for service "%s" ought to have a property "priority"',
+                            $serviceId
+                        )
+                    );
+                }
+                $priorityIsInteger = is_int($tags[0]['priority']);
+                $priorityConsistsOfDigits = is_string($tags[0]['priority']) && ctype_digit($tags[0]['priority']);
+                if (!$priorityIsInteger || $priorityConsistsOfDigits) {
+                    throw new RuntimeException(
+                        sprintf(
+                            'Tag "qa_tools.task_executor" property "priority" of service "%s" ought to be an integer',
+                            $serviceId
+                        )
+                    );
+                }
+
+                return [$serviceId, (int) $tags[0]['priority']];
             },
-            $taggedExecutorIds
+            array_keys($taggedExecutors),
+            $taggedExecutors
+        );
+
+        $sortedExecutorTuples = $executorPriorityTuples;
+        usort(
+            $sortedExecutorTuples,
+            function (array $tupleA, array $tupleB) {
+                return $tupleB[1] - $tupleA[1];
+            }
+        );
+        $taggedExecutorReferences = array_map(
+            function (array $tuple) {
+                return new Reference($tuple[0]);
+            },
+            $sortedExecutorTuples
         );
 
         $executorExecutorDefinition->replaceArgument(0, $taggedExecutorReferences);
--- tests/unit/Core/Application/Compiler/RegisterTaskExecutorsCompilerPassTest.php
+++ tests/unit/Core/Application/Compiler/RegisterTaskExecutorsCompilerPassTest.php
@@ -3,6 +3,7 @@
 namespace Ibuildings\QaTools\UnitTest\Core\Application\Compiler;
 
 use Ibuildings\QaTools\Core\Application\Compiler\RegisterTaskExecutorsCompilerPass;
+use Ibuildings\QaTools\Core\Exception\RuntimeException;
 use Mockery;
 use PHPUnit\Framework\TestCase as TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -15,27 +16,68 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class RegisterTaskExecutorsCompilerPassTest extends TestCase
 {
-    /** @test */
-    public function registers_task_executors()
-    {
-        $executorExecutorDefinition = Mockery::spy(Definition::class);
+    /** @var Definition */
+    private $executorExecutorDefinition;
+    /** @var ContainerBuilder */
+    private $container;
+    /** @var RegisterTaskExecutorsCompilerPass */
+    private $compilerPass;
 
-        $container = Mockery::mock(ContainerBuilder::class);
-        $container
+    protected function setUp()
+    {
+        $this->executorExecutorDefinition = Mockery::spy(Definition::class);
+
+        $this->container = Mockery::mock(ContainerBuilder::class);
+        $this->container
             ->shouldReceive('findDefinition')
             ->with('qa_tools.task.task_directory_executor')
-            ->andReturn($executorExecutorDefinition);
-        $container
+            ->andReturn($this->executorExecutorDefinition);
+
+        $this->compilerPass = new RegisterTaskExecutorsCompilerPass();
+    }
+
+    /** @test */
+    public function registers_task_executors_in_the_order_of_their_priority_when_defined_inorder()
+    {
+        $this->container
             ->shouldReceive('findTaggedServiceIds')
             ->with('qa_tools.task_executor')
-            ->andReturn(['service_a' => [], 'service_b' => []]);
+            ->andReturn(['service_a' => array(['priority' => 10]), 'service_b' => array(['priority' => 0])]);
 
-        $compilerPass = new RegisterTaskExecutorsCompilerPass();
-        $compilerPass->process($container);
+        $this->compilerPass->process($this->container);
 
-        $executorExecutorDefinition
+        $this->executorExecutorDefinition
             ->shouldHaveReceived('replaceArgument')
             ->with(0, Mockery::anyOf([new Reference('service_a'), new Reference('service_b')]))
             ->once();
+    }
+
+    /** @test */
+    public function registers_task_executors_in_the_order_of_their_priority_when_defined_outoforder()
+    {
+        $this->container
+            ->shouldReceive('findTaggedServiceIds')
+            ->with('qa_tools.task_executor')
+            ->andReturn(['service_a' => array(['priority' => 0]), 'service_b' => array(['priority' => 10])]);
+
+        $this->compilerPass->process($this->container);
+
+        $this->executorExecutorDefinition
+            ->shouldHaveReceived('replaceArgument')
+            ->with(0, Mockery::anyOf([new Reference('service_b'), new Reference('service_a')]))
+            ->once();
+    }
+
+    /** @test */
+    public function doesnt_allow_two_tags_per_service()
+    {
+        $this->container
+            ->shouldReceive('findTaggedServiceIds')
+            ->with('qa_tools.task_executor')
+            ->andReturn(['service_a' => array(['priority' => 0], ['priority' => 10])]);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Task executor "service_a" may not be registered more than once');
+        $this->compilerPass->process($this->container);
     }
 }
