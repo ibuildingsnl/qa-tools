84002c126a29e90facd7b5bddbeba32120c0d7d8
Nic Wortel <nic@nicwortel.nl>
2017-01-10 10:57:40 +0100

    Use Interviewer to ask if user wants to replace existing hook

--- src/Core/Application/Command/PreCommitCommand.php
+++ src/Core/Application/Command/PreCommitCommand.php
@@ -2,13 +2,11 @@
 
 namespace Ibuildings\QaTools\Core\Application\Command;
 
-use Ibuildings\QaTools\Core\GitHook\GitHookInstaller;
+use Ibuildings\QaTools\Core\IO\Cli\InterviewerFactory;
 use Ibuildings\QaTools\Core\Project\Directory;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\SymfonyQuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
@@ -26,32 +24,13 @@ final class PreCommitCommand extends Command implements ContainerAwareInterface
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /** @var InterviewerFactory $interviewerFactory */
+        $interviewerFactory = $this->container->get('qa_tools.io.cli.interviewer_factory');
+        $interviewer = $interviewerFactory->createWith($input, $output);
+
         $installer = $this->container->get('qa_tools.git.hook_installer');
-
-        /** @var SymfonyQuestionHelper $questionHelper */
-        $questionHelper = $this->getHelper('question');
-
         $projectRoot = new Directory(getcwd());
 
-        if ($installer->preCommitHookExist($projectRoot)) {
-            $question = new ConfirmationQuestion(
-                '<question>A pre-commit hook already exists in this project. Are you sure you want to overwrite it? '.
-                '(y/N)</question>',
-                false
-            );
-
-            if (!$questionHelper->ask($input, $output, $question)) {
-                $output->writeln(
-                    '<info>The pre-commit hook was left unchanged. You can manually add `ant precommit` to your '.
-                    'pre-commit hook in order to run the pre-commit build before every commit.</info>'
-                );
-
-                return;
-            }
-        }
-
-        $installer->installPreCommitHook($projectRoot);
-
-        $output->writeln('<info>Installed Git pre-commit hook in '.GitHookInstaller::PRE_COMMIT_PATH.'</info>');
+        $installer->installPreCommitHook($interviewer, $projectRoot);
     }
 }
--- src/Core/GitHook/GitHookInstaller.php
+++ src/Core/GitHook/GitHookInstaller.php
@@ -2,6 +2,9 @@
 
 namespace Ibuildings\QaTools\Core\GitHook;
 
+use Ibuildings\QaTools\Core\Interviewer\Answer\YesOrNoAnswer;
+use Ibuildings\QaTools\Core\Interviewer\Interviewer;
+use Ibuildings\QaTools\Core\Interviewer\Question\QuestionFactory;
 use Ibuildings\QaTools\Core\IO\File\FileHandler;
 use Ibuildings\QaTools\Core\Project\Directory;
 
@@ -19,19 +22,32 @@ final class GitHookInstaller
         $this->fileHandler = $fileHandler;
     }
 
-    /**
-     * @return bool
-     */
-    public function preCommitHookExist(Directory $projectRoot)
-    {
-        return $this->fileHandler->exists($projectRoot->getDirectory().self::PRE_COMMIT_PATH);
-    }
-
-    public function installPreCommitHook(Directory $projectRoot)
+    public function installPreCommitHook(Interviewer $interviewer, Directory $projectRoot)
     {
         $filePath = $projectRoot->getDirectory().self::PRE_COMMIT_PATH;
 
+        if ($this->fileHandler->exists($filePath)) {
+            /** @var YesOrNoAnswer $overwrite */
+            $overwrite = $interviewer->ask(
+                QuestionFactory::createYesOrNo(
+                    'A pre-commit hook already exists in this project. Are you sure you want to overwrite it?',
+                    YesOrNoAnswer::NO
+                )
+            );
+
+            if ($overwrite->is(false)) {
+                $interviewer->notice(
+                    'The pre-commit hook was left unchanged. You can manually add `ant precommit` to your '.
+                    'pre-commit hook in order to run the pre-commit build before every commit.'
+                );
+
+                return;
+            }
+        }
+
         $this->fileHandler->writeTo($filePath, $this->fileHandler->readFrom(__DIR__.'/files/pre-commit'));
         $this->fileHandler->changeMode($filePath, 0775);
+
+        $interviewer->success(sprintf('Installed Git pre-commit hook in %s.', $filePath));
     }
 }
