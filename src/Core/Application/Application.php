9291df59dbcbf4ac7c50bd54a6325219bd85e47d
Nic Wortel <nic@nicwortel.nl>
2017-01-11 14:46:38 +0100

    Suppress PHPMD warning

--- src/Core/Application/Application.php
+++ src/Core/Application/Application.php
@@ -17,6 +17,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 final class Application extends ConsoleApplication
 {
     const NAME = 'Ibuildings QA Tools';
