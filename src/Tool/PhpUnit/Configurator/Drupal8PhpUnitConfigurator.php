0c6254b588b00905e71c4215bc12b2d851b98237
Reinier Kip <rkip@ibuildings.nl>
2017-01-20 15:09:41 +0100

    Bootstrap Drupal 8 projects with custom PHPUnit bootstrap file

---
+++ src/Tool/PhpUnit/Configurator/Drupal8PhpUnitConfigurator.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Ibuildings\QaTools\Tool\PhpUnit\Configurator;
+
+use Ibuildings\QaTools\Core\Assert\Assertion;
+use Ibuildings\QaTools\Core\Build\Build;
+use Ibuildings\QaTools\Core\Build\Snippet;
+use Ibuildings\QaTools\Core\Build\Tool;
+use Ibuildings\QaTools\Core\Configuration\TaskDirectory;
+use Ibuildings\QaTools\Core\Configuration\TaskHelperSet;
+use Ibuildings\QaTools\Core\Configurator\Configurator;
+use Ibuildings\QaTools\Core\Interviewer\Answer\YesOrNoAnswer;
+use Ibuildings\QaTools\Core\Interviewer\Interviewer;
+use Ibuildings\QaTools\Core\Interviewer\Question\QuestionFactory;
+use Ibuildings\QaTools\Core\Task\AddAntBuildTask;
+use Ibuildings\QaTools\Core\Task\InstallComposerDevDependencyTask;
+use Ibuildings\QaTools\Core\Task\WriteFileTask;
+use Ibuildings\QaTools\Tool\PhpUnit\PhpUnit;
+
+final class Drupal8PhpUnitConfigurator implements Configurator
+{
+    /**
+     * @var string
+     */
+    private $phpunitConfigurationTemplate;
+
+    public function __construct($phpunitConfigurationTemplate)
+    {
+        Assertion::string($phpunitConfigurationTemplate);
+
+        $this->phpunitConfigurationTemplate = $phpunitConfigurationTemplate;
+    }
+
+    public function configure(Interviewer $interviewer, TaskDirectory $taskDirectory, TaskHelperSet $taskHelperSet)
+    {
+        $usePhpUnit = $interviewer->ask(
+            QuestionFactory::createYesOrNo(
+                'Would you like to install PHPUnit for running automated tests?',
+                YesOrNoAnswer::YES
+            )
+        );
+
+        /** @var YesOrNoAnswer $usePhpUnit */
+        if ($usePhpUnit->is(YesOrNoAnswer::NO)) {
+            return;
+        }
+
+        $taskDirectory->registerTask(new InstallComposerDevDependencyTask('phpunit/phpunit', '^5.7'));
+
+        $taskDirectory->registerTask(
+            new WriteFileTask(
+                $taskDirectory->getProject()->getConfigurationFilesLocation()->getDirectory() . 'phpunit.xml',
+                $taskHelperSet->renderTemplate($this->phpunitConfigurationTemplate)
+            )
+        );
+
+        $taskDirectory->registerTask(
+            new WriteFileTask(
+                $taskDirectory->getProject()->getConfigurationFilesLocation()->getDirectory() . 'tests/bootstrap.php',
+                $taskHelperSet->renderTemplate('drupal8/bootstrap.php.twig')
+            )
+        );
+
+        $antSnippet = $taskHelperSet->renderTemplate(
+            'ant-build.xml.twig',
+            ['targetName' => PhpUnit::ANT_TARGET]
+        );
+
+        $taskDirectory->registerTask(
+            new AddAntBuildTask(
+                Build::main(),
+                Tool::withIdentifier(PhpUnit::ANT_TARGET),
+                Snippet::withContentsAndTargetName($antSnippet, PhpUnit::ANT_TARGET)
+            )
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getToolClassName()
+    {
+        return PhpUnit::class;
+    }
+}
---
+++ tests/unit/Tool/PhpUnit/Configurator/Drupal8PhpUnitConfiguratorTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Ibuildings\QaTools\UnitTest\Tool\PhpUnit\Configurator;
+
+use Ibuildings\QaTools\Core\Build\Build;
+use Ibuildings\QaTools\Core\Build\Snippet;
+use Ibuildings\QaTools\Core\Build\Tool;
+use Ibuildings\QaTools\Core\Configuration\TaskDirectory;
+use Ibuildings\QaTools\Core\Configuration\TaskHelperSet;
+use Ibuildings\QaTools\Core\Interviewer\Answer\YesOrNoAnswer;
+use Ibuildings\QaTools\Core\Interviewer\AutomatedResponseInterviewer;
+use Ibuildings\QaTools\Core\Project\Directory;
+use Ibuildings\QaTools\Core\Project\Project;
+use Ibuildings\QaTools\Core\Project\ProjectTypeSet;
+use Ibuildings\QaTools\Tool\PhpUnit\Configurator\Drupal8PhpUnitConfigurator;
+use Ibuildings\QaTools\UnitTest\AddBuildTaskMatcher;
+use Ibuildings\QaTools\UnitTest\InstallComposerDevDependencyTaskMatcher;
+use Ibuildings\QaTools\UnitTest\WriteFileTaskMatcher;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class Drupal8PhpUnitConfiguratorTest extends PHPUnit_Framework_TestCase
+{
+    const TARGET_NAME = 'phpunit';
+
+    /**
+     * @var AutomatedResponseInterviewer
+     */
+    private $interviewer;
+
+    /**
+     * @var Mockery\MockInterface|TaskDirectory
+     */
+    private $taskDirectory;
+
+    /**
+     * @var Mockery\MockInterface|TaskHelperSet
+     */
+    private $taskHelperSet;
+
+    protected function setUp()
+    {
+        $this->interviewer = new AutomatedResponseInterviewer();
+
+        $this->taskDirectory = Mockery::spy(TaskDirectory::class);
+        $this->taskDirectory->shouldReceive('getProject')->andReturn(
+            new Project(
+                'Xenophobic Xavier',
+                new Directory('.'),
+                new Directory('.'),
+                new ProjectTypeSet(),
+                false
+            )
+        );
+        $this->taskHelperSet = Mockery::mock(TaskHelperSet::class);
+    }
+
+    /**
+     * @test
+     */
+    public function installs_phpunit_when_desired()
+    {
+        $this->interviewer->recordAnswer(
+            'Would you like to install PHPUnit for running automated tests?',
+            YesOrNoAnswer::yes()
+        );
+
+        $this->taskHelperSet
+            ->shouldReceive('renderTemplate')
+            ->with('phpunit.xml.twig')
+            ->andReturn('<?xml version="1.0" encoding="UTF-8"?>');
+
+        $this->taskHelperSet
+            ->shouldReceive('renderTemplate')
+            ->with('drupal8/bootstrap.php.twig')
+            ->andReturn('<?php bootstrap();');
+
+        $this->taskHelperSet
+            ->shouldReceive('renderTemplate')
+            ->with('ant-build.xml.twig', ['targetName' => self::TARGET_NAME])
+            ->andReturn('phpunit-snippet');
+
+        $configurator = new Drupal8PhpUnitConfigurator('phpunit.xml.twig');
+        $configurator->configure($this->interviewer, $this->taskDirectory, $this->taskHelperSet);
+
+        $this->taskDirectory->shouldHaveReceived(
+            'registerTask',
+            [InstallComposerDevDependencyTaskMatcher::forVersionOf('phpunit/phpunit', '^5.7')]
+        );
+
+        $this->taskDirectory
+            ->shouldHaveReceived('registerTask')
+            ->with(WriteFileTaskMatcher::contains('./phpunit.xml', '<?xml version="1.0" encoding="UTF-8"?>'))
+            ->once();
+
+        $this->taskDirectory
+            ->shouldHaveReceived('registerTask')
+            ->with(WriteFileTaskMatcher::contains('./tests/bootstrap.php', '<?php bootstrap();'))
+            ->once();
+
+        $this->taskDirectory->shouldHaveReceived(
+            'registerTask',
+            [
+                AddBuildTaskMatcher::with(
+                    Build::main(),
+                    Tool::withIdentifier(self::TARGET_NAME),
+                    Snippet::withContentsAndTargetName('phpunit-snippet', self::TARGET_NAME)
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function does_not_install_phpunit_when_not_desired()
+    {
+        $this->interviewer->recordAnswer(
+            'Would you like to install PHPUnit for running automated tests?',
+            YesOrNoAnswer::no()
+        );
+
+        $configurator = new Drupal8PhpUnitConfigurator('phpunit.xml.twig');
+        $configurator->configure($this->interviewer, $this->taskDirectory, $this->taskHelperSet);
+
+        $this->taskDirectory->shouldNotHaveReceived('registerTask');
+    }
+}
