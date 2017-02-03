a810f2ea2a2962e2021096f1dc904744aea6e01b
Reinier Kip <rkip@ibuildings.nl>
2017-02-03 15:19:22 +0100

    System-test that the correct files are used with various tools

--- tests/system/specs/003_can-configure-a-symfony-project.tcl
+++ tests/system/specs/003_can-configure-a-symfony-project.tcl
@@ -2,12 +2,12 @@ test ./qa-tools configure --no-ansi -v
 
 should_see "Configuring the Ibuildings QA Tools"
 
-answer "What is the project's name?" with "Drupal Dope"
+answer "What is the project's name?" with "Symfony Success"
 
 answer "Where would you like to store the generated files?" with "./"
 
 should_see "What type of project would you like to configure?"
-answer "\[3\] Drupal 8" with "3"
+answer "\[1\] Symfony 3" with "1"
 
 answer "Would you like to integrate Travis in your project?" with "n"
 
